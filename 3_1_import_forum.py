fd348e4c23d8381bc61ef2e7457b7c7caaa5e597
50769pki <kievits@rsm.nl>
2025-12-23 14:36:56 +0100

    Imported discourse data

--- 3_1_import_forum.py
+++ 3_1_import_forum.py
@@ -1,3 +1,4 @@
+import json
 import math
 import os
 import pandas as pd
@@ -6,6 +7,9 @@ import re
 import requests
 import time
 import validators
+
+from bs4 import BeautifulSoup
+from seleniumbase import SB
 
 
 # load propositions
@@ -45,5 +49,77 @@ def check_urls():
             urls.to_csv(filename, index=False)
             time.sleep(random.randrange(10, 15))
 
+
+def scarpe_selenium():
+    urls = get_urls()
+
+    if 'selenium_body' not in urls:
+        urls['selenium_body'] = None
+
+    for url in urls.itertuples():
+        if url.exists and (not url.selenium_body or (type(url.selenium_body) != str and math.isnan(url.selenium_body))):
+            print(url.url)
+            with SB(uc=True, headless=True) as driver:
+                driver.uc_open_with_reconnect(url.url, 4)
+                
+                try:
+                    driver.assert_element("section#main", timeout=3)
+                    page_height = driver.get_window_size()["height"]
+
+                    while True:
+                        print(page_height)
+                        driver.scroll_to_bottom()
+                        time.sleep(2)
+                        if driver.get_window_size()["height"] > page_height:
+                            page_height = driver.get_window_size()["height"]
+                        else:
+                            break
+                except:
+                    pass
+
+                page_source = driver.execute_script("return document.documentElement.outerHTML;")
+                urls.at[url.Index, 'selenium_body'] = page_source
+
+            urls.to_csv(filename, index=False)
+            time.sleep(random.randrange(10, 15))
+
+
+def get_discourse_data():
+    urls = get_urls()
+    urls["is_selenium"] = False
+    urls["is_discourse"] = False
+    urls["discourse_json"] = False
+    for url in urls.itertuples():
+        if url.exists and type(url.selenium_body) == str:
+            urls.at[url.Index, 'is_selenium'] = True
+            if "<discourse-assets>" in url.selenium_body:
+                soup = BeautifulSoup(url.selenium_body, 'html.parser')
+                data = json.loads(soup.select("discourse-assets-json > div")[0]["data-preloaded"])
+                keys = [k for k in list(data.keys()) if k[:6] == "topic_"]
+                if len(keys) > 0:
+                    data = json.loads(data[keys[0]])
+                    urls.at[url.Index, 'is_discourse'] = True
+                    urls.at[url.Index, 'discourse_json'] = json.dumps(data)
+
+    urls.to_csv(filename, index=False)
+
+
+def filter_usable_data():
+    urls = get_urls()
+    urls = urls.loc[urls["is_discourse"] == True]
+    urls = urls[["url", "selenium_body", "discourse_json"]]
+    urls = urls.rename(columns={"selenium_body": "html", "discourse_json": "json"})
+    urls.to_csv("csvs/discourse.csv", index=False)
+
+    for i, url in enumerate(urls.itertuples()):
+        data = json.loads(url.discourse_json)
+        data["url"] = url.url
+        with open(f"json/{i + 1}.json", "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file)
+
+
 if __name__ == "__main__":
     check_urls()
+    scarpe_selenium()
+    get_discourse_data()
+    filter_usable_data()
